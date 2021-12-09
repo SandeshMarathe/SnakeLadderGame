4b6cafb9d20b5a3cb40620b67e33b95fc371bc04
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-09 06:26:35 +0530

    ADDED : Game Played with Single Player at Start Position 0

---
+++ SnakeLadderGameUC.java
@@ -0,0 +1,9 @@
+public class SnakeLadderGameUC
+{
+	public static void main(String args[])
+	{
+		int singlePlayer=0;
+
+		System.out.println("Single Player at Start Position "+singlePlayer);
+	}
+}
